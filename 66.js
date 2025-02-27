901c46c4dbccb743d2cf0b5dcb0039b873350398
sulman <msulmmehmood@gmail.com>
2025-02-27 11:16:06 -0800

    learn iterables

--- 66.js
+++ 66.js
@@ -1,1 +1,26 @@
-console.log("this is 66 no file")
+//  iterables
+//  jispe hum for of loop laga sakein
+//  String, array are iterable
+
+ 
+
+
+
+// const items=["item1","item2","item3"];
+// for(let item of items){
+//     console.log(item);
+// }
+
+
+
+
+//  array like object 
+//  jinke pas length property hoti ha?
+//  aur jinko hum index se access kar sakte hai
+//  example :- string
+
+
+const firstName = "harshit";
+console.log(firstName.length);
+console.log(firstName[3]);
+
